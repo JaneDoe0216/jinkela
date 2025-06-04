4a23c21abcf41ee83cef643ba394338a840fc57f
Hyacine <hyacinesweetie@outlook.com>
2025-06-04 05:24:50 +0000

    Templete updated.

--- app/main.tsx
+++ app/main.tsx
@@ -1,14 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router'
 
-import './main.css'
-import App from './App.tsx'
+import '@/main.css'
+import router from '@/routes/index'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <RouterProvider router={router} />
     </StrictMode>
 )
